7b3f6d7338e96f7918bcd3746bef6d26f5640a79
Lucas Tormin <lucasntormin@gmail.com>
2016-11-04 17:37:40 -0200

    Corrigido caracteres inválidos que não permitiam o build corretamente.

--- project/src/apresentacao/TelaRelatorioChamado.java
+++ project/src/apresentacao/TelaRelatorioChamado.java
@@ -16,95 +16,95 @@ import javax.swing.JTextArea;
 
 import controle.ControleChamados;
 
-public class TelaRelatorioChamado extends JFrame{
+public class TelaRelatorioChamado extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private ControleChamados CC;
-	private JLabel lEmitir;	
-	private JButton btEmitir;
-	private JButton btApagar;
-	private JTextArea arearelatorios;
-	private JComboBox<String> lstProb;
-	private Container container;
-	private GE gerenciador;
-	
-	public TelaRelatorioChamado(ControleChamados c){	
-		super("RelatÛrio de chamados encerrados");
-		this.CC = c;
-		container = getContentPane();
-		container.setLayout(new GridBagLayout());		
-		setSize(700, 700);
-		gerenciador = new GE();
-		inicializar();
-	}
-	
-	private void inicializar(){
-		
-		GridBagConstraints cts = new GridBagConstraints();
-				
-		cts.fill = GridBagConstraints.HORIZONTAL;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private ControleChamados CC;
+    private JLabel lEmitir;
+    private JButton btEmitir;
+    private JButton btApagar;
+    private JTextArea arearelatorios;
+    private JComboBox<String> lstProb;
+    private Container container;
+    private GE gerenciador;
+
+    public TelaRelatorioChamado(ControleChamados c) {
+        super("Relat√≥rio de chamados encerrados");
+        this.CC = c;
+        container = getContentPane();
+        container.setLayout(new GridBagLayout());
+        setSize(700, 700);
+        gerenciador = new GE();
+        inicializar();
+    }
+
+    private void inicializar() {
+
+        GridBagConstraints cts = new GridBagConstraints();
+
+        cts.fill = GridBagConstraints.HORIZONTAL;
         cts.gridx = 0;
         cts.gridy = 1;
-		container.add(new JLabel("Selecione tipo de problema:"), cts);
-		
-		//instancia, adiciona valores e adiciona combo
-		lstProb = new JComboBox<String>();
-		lstProb.addItem("Problema de Rede/Conex„o");
-		lstProb.addItem("Problema de Banco de Dados");
-		lstProb.addItem("Problema de desempenho");
-		cts.fill = GridBagConstraints.HORIZONTAL;
+        container.add(new JLabel("Selecione tipo de problema:"), cts);
+
+        //instancia, adiciona valores e adiciona combo
+        lstProb = new JComboBox<String>();
+        lstProb.addItem("Problema de Rede/Conex√£o");
+        lstProb.addItem("Problema de Banco de Dados");
+        lstProb.addItem("Problema de desempenho");
+        cts.fill = GridBagConstraints.HORIZONTAL;
         cts.gridx = 0;
         cts.gridy = 2;
-		container.add(lstProb, cts);
-		
-		//instancia e adiciona area de texto
-		arearelatorios = new JTextArea();
-		arearelatorios.setPreferredSize(new Dimension(500, 500));		
-		cts.fill = GridBagConstraints.HORIZONTAL;
+        container.add(lstProb, cts);
+
+        //instancia e adiciona area de texto
+        arearelatorios = new JTextArea();
+        arearelatorios.setPreferredSize(new Dimension(500, 500));
+        cts.fill = GridBagConstraints.HORIZONTAL;
         cts.gridx = 0;
         cts.gridy = 3;
-		container.add(arearelatorios, cts);
-		
-		//instancia e adiciona botoes
-		btApagar = new JButton("Apagar");
-		btApagar.setActionCommand(btApagar.getText());
-		btApagar.addActionListener(gerenciador);
-		cts.fill = GridBagConstraints.HORIZONTAL;
+        container.add(arearelatorios, cts);
+
+        //instancia e adiciona botoes
+        btApagar = new JButton("Apagar");
+        btApagar.setActionCommand(btApagar.getText());
+        btApagar.addActionListener(gerenciador);
+        cts.fill = GridBagConstraints.HORIZONTAL;
         cts.gridx = 0;
         cts.gridy = 5;
-		container.add(btApagar, cts);
-		
-		btEmitir = new JButton("Emitir relatÛrios");
-		btEmitir.setActionCommand(btEmitir.getText());
-		btEmitir.addActionListener(gerenciador);
-		cts.fill = GridBagConstraints.HORIZONTAL;
+        container.add(btApagar, cts);
+
+        btEmitir = new JButton("Emitir relat√≥rios");
+        btEmitir.setActionCommand(btEmitir.getText());
+        btEmitir.addActionListener(gerenciador);
+        cts.fill = GridBagConstraints.HORIZONTAL;
         cts.gridx = 0;
         cts.gridy = 4;
-		container.add(btEmitir, cts);
-	}
-	
-	public void incluirTexto(String txt){
-		arearelatorios.setText(txt);
-	}
-	
-	private class GE implements ActionListener{
+        container.add(btEmitir, cts);
+    }
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(e.getActionCommand().equals(btEmitir.getText())){
-				String relatorio = CC.emitirRelatorios(lstProb.getSelectedIndex());
-				if(relatorio.equals("")){ incluirTexto("N„o h· chamados deste tipo de problema para exibir");
-				}else{
-					incluirTexto(CC.emitirRelatorios(lstProb.getSelectedIndex()));
-				}
-			}
-			else if(e.getActionCommand().equals(btApagar.getActionCommand())){
-				incluirTexto("");
-			}
-		}
-		
-	}
+    public void incluirTexto(String txt) {
+        arearelatorios.setText(txt);
+    }
+
+    private class GE implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getActionCommand().equals(btEmitir.getText())) {
+                String relatorio = CC.emitirRelatorios(lstProb.getSelectedIndex());
+                if (relatorio.equals("")) {
+                    incluirTexto("N√£o h√° chamados deste tipo de problema para exibir");
+                } else {
+                    incluirTexto(CC.emitirRelatorios(lstProb.getSelectedIndex()));
+                }
+            } else if (e.getActionCommand().equals(btApagar.getActionCommand())) {
+                incluirTexto("");
+            }
+        }
+
+    }
 }
